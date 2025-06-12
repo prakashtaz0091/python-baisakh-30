f6ebee729fb49a79dd27336f6205077c611328ea
Prakash Tajpuriya <86336006+prakashtaz0091@users.noreply.github.com>
2025-06-12 22:18:10 +0545

    Web scraping from api | scheduling tasks

---
+++ python baisakh 30/web_scrapping/scrape_api.py
@@ -0,0 +1,73 @@
+import requests
+import csv
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+import schedule
+import time
+
+load_dotenv()
+
+
+def scrape():
+    url = "https://real-time-amazon-data.p.rapidapi.com/deals-v2"
+
+    querystring = {
+        "country": "US",
+        "min_product_star_rating": "ALL",
+        "price_range": "ALL",
+        "discount_range": "ALL",
+    }
+
+    headers = {
+        "x-rapidapi-key": os.getenv("X-RAPID-API-KEY"),
+        "x-rapidapi-host": os.getenv("X-RAPID-API-HOST"),
+    }
+
+    output_filename = (
+        f"./outputs/amazon-deals-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.csv"
+    )
+
+    deals_data = []
+
+    response = requests.get(url, headers=headers, params=querystring)
+    response = response.json()
+    if response["status"] == "OK":
+        deals = response["data"]["deals"]
+        for deal in deals:
+            deal_title = deal["deal_title"]
+            deal_url = deal["deal_url"]
+            deal_photo = deal["deal_photo"]
+            deal_price = deal["deal_price"]
+
+            new_deal = [
+                deal_title,
+                deal_url,
+                deal_photo,
+                deal_price,
+            ]
+            deals_data.append(new_deal)
+
+        with open(output_filename, "w") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                [
+                    "Title",
+                    "URL",
+                    "Photo",
+                    "Price",
+                ]
+            )
+            writer.writerows(deals_data)
+
+        print("scrape success")
+    else:
+        print("Something went wrong!")
+
+
+schedule.every(10).seconds.do(scrape)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
